7159b193c16358bc60133dff1a1a0dc95a5b530a
Quaid Tahir <quaidcodes@gmail.com>
2025-05-29 10:38:45 -0400

    homepage build author change

--- quaid-tahir-portfolio/src/app/page.tsx
+++ quaid-tahir-portfolio/src/app/page.tsx
@@ -1,12 +1,13 @@
 
 
+import Header from "@/components/Header";
+
+
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
-      <button className="px-5 py-1 ml-10 mt-10 bg-blue-400 text-white font-bold rounded-full border-2 border-transparent hover:bg-white hover:border-2 hover:border-black hover:text-black">Button!</button>
 
-    </main>
-  )
+  return <>
+    <Header />
+    
+  </>
 }
---
+++ quaid-tahir-portfolio/src/components/Header.tsx
@@ -0,0 +1,30 @@
+export default function Header() {
+  return (
+    <>
+      <header className="w-full bg-">
+        <nav>
+          <ul className="flex justify-center">
+            <li className="nav-link">
+              <a href="">Quaid Tahir</a>
+            </li>
+
+            <div>
+              <li className="nav-link">
+                <a href="">Projects</a>
+              </li>
+              <li className="nav-link">
+                <a href="">Portfolio</a>
+              </li>
+              <li className="nav-link">
+                <a href="">Blogs</a>
+              </li>
+              <li className="nav-link">
+                <a href="">Contact</a>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
