a03091b0814b65a09b2b0897908cfe18e604a1a6
Quaid Tahir <quaidcodes@gmail.com>
2025-06-14 05:06:57 -0400

    updating dependencies

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import PdfViewer from "@/components/PdfViewer";
-
 export default function Portfolio() {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-6">My Portfolio PDF</h1>
-      <PdfViewer fileUrl="/QuaidTahirResume.pdf" />
     </div>
   );
 }
