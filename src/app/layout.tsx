37faa75c9676966225cd168f8b2e7bbd673bd8db
Quaid Tahir <quaidcodes@gmail.com>
2025-06-15 00:30:41 -0400

    remove hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import Header from "@/components/Header";
-import Hero from "@/components/Hero";
 
 import "@/styles/globals.css";
 import Footer from "@/components/Footer";
@@ -63,7 +62,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
       >
         <Header />
-        {/* <Hero /> */}
         <Footer />
         {children}
       </body>
