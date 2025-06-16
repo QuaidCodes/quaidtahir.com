996a3d95c1756a510787c8c76c9917e923d03a85
Quaid Tahir <quaidcodes@gmail.com>
2025-06-15 23:26:48 -0400

    remove unsused variables

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import {motion} from 'framer-motion';
+// import {motion} from 'framer-motion';
 import { z } from "zod";
 
 export default function Contact() {
