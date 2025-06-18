389414bcfe2896b29cc664124514bb0930cb82b4
Quaid Tahir <quaidcodes@gmail.com>
2025-06-18 04:48:16 -0400

    spotlight

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,33 +1,18 @@
+import { Spotlight } from "@/components/ui/spotlight-new";
+
 export default function Hero() {
-// https://ui.aceternity.com/components/lamp-effect as loading for new pages?
-// https://ui.aceternity.com/components/placeholders-and-vanish-input for search options on portfolio site and other sites.
-// https://ui.aceternity.com/components/sparkles landing page
-// https://ui.aceternity.com/components/text-generate-effect on main page maybe about me.
-// https://www.hyperui.dev/components/marketing/headers for headers.
-// https://ui.aceternity.com/components/timeline this is timeline add this 100%.
-// https://www.hyperui.dev/components/application/timelines another timeline.
-// https://ui.shadcn.com/docs/components/carousel maybe for photos or projects? 
-// https://ui.shadcn.com/docs/components/sidebar sidebar for saas
-// https://ui.shadcn.com/docs/components/checkbox for saas
-// https://ui.shadcn.com/charts/radar#charts skills chart
-// https://uiverse.io/Praashoo7/quick-fish-43 for socials?
-// https://www.heroui.com/docs/components/pagination for pagination on papers page.
-// https://www.heroui.com/docs/components/range-calendar calender for site.
-// https://www.heroui.com/docs/components/slider for sliders.
-// https://www.heroui.com/docs/components/snippet for copy.
-// https://www.heroui.com/docs/components/switch switches for toggle buttons.
-// https://www.hyperui.dev/components/marketing/pricing for pricing.
-// https://www.hyperui.dev/components/application/steps this is for progress for completing forms.
-// https://www.hyperui.dev/components/marketing/carts for checkout carts.
-// https://www.hyperui.dev/components/marketing/faqs frequently asked questions.
+  return (
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight />
 
-
-    return (
-    <div className="pb-20 pt-36">
-      <div>
-         
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Quaid Tahir
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          Software Engineer
+        </p>
       </div>
     </div>
   );
 }
-
