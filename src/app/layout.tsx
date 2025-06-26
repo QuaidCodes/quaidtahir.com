779c4fe5e029e6a8b298f51ac84136d86d97c336
Tahir <quaidcodes@gmail.com>
2025-06-26 13:36:57 -0400

    adding preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,22 @@ export const metadata: Metadata = {
       "Portfolio site of Quaid Tahir, showcasing projects and skills.",
     url: "www.quaidtahir.com",
     siteName: "Quaid Tahir Portfolio",
+    images: [
+      {
+        url: "public/www.quaidtahir.com_preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Screenshot of my portfolio site",
+      },
+    ],
     locale: "en_US",
     type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Quaid Tahir Portfolio",
+    description: "Portfolio site of Quaid Tahir, showcasing projects and skills.",
+    images: ["public/www.quaidtahir.com_preview.png"],
   },
 };
 
